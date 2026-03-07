20635d2b770c475e12ccc791dd202d52dcd3dd74
osky562-del <osky562@gmail.com>
2026-03-07 14:43:30 +0100

    Eliminar firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBphY_jaOOOVOzwzfoXE78_iDVOQYRhA2o",
-  authDomain: "control-de-gastos-39154.firebaseapp.com",
-  projectId: "control-de-gastos-39154",
-  storageBucket: "control-de-gastos-39154.firebasestorage.app",
-  messagingSenderId: "845236944662",
-  appId: "1:845236944662:web:897201644eaf708afd5a37",
-  measurementId: "G-LRPGXQYBLK"
-};
