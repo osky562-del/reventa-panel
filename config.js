d737b998194640c1d235d2625f7b2e3c6de4d2c1
osky562-del <osky562@gmail.com>
2026-03-09 23:23:52 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXnKRYZ_1BLKEK9T50d85Gzzf3so2DDbM",
+    databaseURL: "https://mi-cuadrante-furgo-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mi-cuadrante-furgo"
+};
