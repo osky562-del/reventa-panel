8c4b42d4827f99f82f3a180ae1ba44daf1861d76
osky562-del <osky562@gmail.com>
2026-03-09 23:39:21 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDXnKRYZ_1BLKEK9T50d85Gzzf3so2DDbM",
-    databaseURL: "https://mi-cuadrante-furgo-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mi-cuadrante-furgo"
-};
